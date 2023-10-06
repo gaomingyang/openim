396847d65bda89998462c77c110873adc2ed3749
Mingyang Gao <gaomingyangcn@gmail.com>
2023-10-05 22:22:07 -0600

    move router into router folder

--- go.mod
+++ go.mod
@@ -12,6 +12,7 @@ require (
 	github.com/go-sql-driver/mysql v1.7.0
 	github.com/google/uuid v1.3.0
 	github.com/gorilla/websocket v1.5.0
+	github.com/rs/cors v1.10.1
 	github.com/spf13/viper v1.16.0
 	gorm.io/driver/mysql v1.5.1
 	gorm.io/gorm v1.25.1
@@ -20,14 +21,15 @@ require (
 require (
 	github.com/atotto/clipboard v0.1.4 // indirect
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
-	github.com/bytedance/sonic v1.9.1 // indirect
-	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
+	github.com/bytedance/sonic v1.10.1 // indirect
+	github.com/chenzhuoyu/base64x v0.0.0-20230717121745-296ad89f973d // indirect
+	github.com/chenzhuoyu/iasm v0.9.0 // indirect
 	github.com/containerd/console v1.0.4-0.20230313162750-1ae8d489ac81 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
-	github.com/go-playground/validator/v10 v10.14.1 // indirect
+	github.com/go-playground/validator/v10 v10.15.5 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
@@ -47,8 +49,9 @@ require (
 	github.com/muesli/cancelreader v0.2.2 // indirect
 	github.com/muesli/reflow v0.3.0 // indirect
 	github.com/muesli/termenv v0.15.2 // indirect
-	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
+	github.com/pelletier/go-toml/v2 v2.1.0 // indirect
 	github.com/rivo/uniseg v0.4.4 // indirect
+	github.com/rs/cors/wrapper/gin v0.0.0-20230929213613-e19471c92b1c // indirect
 	github.com/spf13/afero v1.9.5 // indirect
 	github.com/spf13/cast v1.5.1 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
@@ -56,14 +59,14 @@ require (
 	github.com/subosito/gotenv v1.4.2 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.11 // indirect
-	golang.org/x/arch v0.3.0 // indirect
-	golang.org/x/crypto v0.10.0 // indirect
-	golang.org/x/net v0.11.0 // indirect
+	golang.org/x/arch v0.5.0 // indirect
+	golang.org/x/crypto v0.14.0 // indirect
+	golang.org/x/net v0.16.0 // indirect
 	golang.org/x/sync v0.3.0 // indirect
-	golang.org/x/sys v0.10.0 // indirect
-	golang.org/x/term v0.10.0 // indirect
-	golang.org/x/text v0.11.0 // indirect
-	google.golang.org/protobuf v1.30.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+	golang.org/x/term v0.13.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -2,17 +2,14 @@ package main
 
 import (
 	"flag"
-	"fmt"
-	"github.com/gin-contrib/cors"
-	"github.com/gin-gonic/gin"
-	"github.com/spf13/viper"
 	"log"
-	"net/http"
 	"openim/config"
 	"openim/dao"
-	"openim/services"
+	"openim/router"
 	"openim/ws"
 	"os"
+
+	"github.com/spf13/viper"
 )
 
 // var http_addr = flag.String("http_addr", ":8001", "http service address")
@@ -44,84 +41,6 @@ func main() {
 	// go im.StartSocketServer()
 	go ws.StartWsServer()
 
-	r := gin.Default()
-	corsMiddleWare := cors.Default()
-	r.Use(corsMiddleWare)
-	r.GET("/ping", pong)
-
-	// apis
-	r.POST("/register", services.UserRegister)
-	r.POST("/login", services.UserLogin)
-
-	// group
-	r.GET("/groups", services.OpenGroups)           // 查看所有开放的组列表
-	r.POST("/group/apply", services.ApplyJoinGroup) // 申请入群
-	r.GET("/my/group/list", services.MyGroupList)   // 查看自己的组列表
-	r.GET("/group/info", services.GroupInfo)        // 查看某个组的信息
-	r.GET("/group/members", services.GroupMembers)
-	r.POST("/group/create", services.CreateGroup)
-	r.POST("/group/join", services.JoinGroup)
-	r.POST("/group/quit", services.QuitGroup)
-
-	// friends
-	// r.GET("/my/friends")
-
-	r.LoadHTMLGlob("public/*")
-	r.GET("/manage", managePage)
-	r.GET("/chat", chatPage)
-	r.GET("/login", loginPage)
+	r := router.SetupRouter()
 	r.Run(viper.GetString("apiPort")) // listen and serve on 0.0.0.0:8080
-
-	// log.Println("Start web server with port number", *http_addr)
-	// http.HandleFunc("/version", version)
-	// // http.Handle("/", http.FileServer(http.Dir("./public")))  //根目录指向public
-	// http.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("./public"))))
-	//
-	// // 开启web 聊天页面
-	// http.HandleFunc("/", home)
-
-	// err := http.ListenAndServe(PORT, nil)
-	// flag获取的addr是一个指针，对指针使用*进行取值
-	// err := http.ListenAndServe(*http_addr, nil)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
-}
-
-func pong(c *gin.Context) {
-	// c.String(200, "pong")
-	c.JSON(200, gin.H{"message": "pong"})
-}
-
-func version(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "0.0.1")
-}
-
-func home(w http.ResponseWriter, r *http.Request) {
-	log.Println("Serving", r.Host, "for", r.URL.Path)
-	log.Println(r.URL)
-	if r.URL.Path != "/" {
-		http.Error(w, "Not found", http.StatusNotFound)
-		return
-	}
-	if r.Method != "GET" {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-	http.ServeFile(w, r, "./public/home.html")
-}
-
-// todo this page could use ws update number instead of refresh webpage by hand
-func managePage(c *gin.Context) {
-	onlineUserNumber := ws.GetOnlineUserNumber()
-	log.Println("online UserNumber:", onlineUserNumber)
-	c.HTML(http.StatusOK, "manage.html", gin.H{"onlineUserNumber": onlineUserNumber})
-}
-
-func chatPage(c *gin.Context) {
-	c.HTML(http.StatusOK, "chat.html", nil)
-}
-
-func loginPage(c *gin.Context) {
-	c.HTML(http.StatusOK, "login.html", nil)
 }
